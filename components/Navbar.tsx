de58483ebf812cdbcfc72e7549f60626c154e7ae
kennyflaten <kennyflaten@protonmail.com>
2022-08-05 21:52:36 -0400

    navbar and globals

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -100,7 +100,7 @@ const Navbar: FC = () => {
   }, [filterableCollection])
 
   return (
-    <nav className="sticky top-0 z-[1000] col-span-full mb-[10px] flex items-center justify-between gap-2 bg-white px-6 py-4 dark:border-neutral-600 dark:bg-[#161616] md:gap-3 md:py-3 md:px-8">
+    <nav className="sticky top-0 z-[1000] col-span-full mb-[0px] flex items-center justify-between gap-2 bg-white px-6 py-4 dark:border-neutral-600 dark:bg-[#161616] md:gap-3 md:py-3 md:px-8">
       <NavbarLogo className="z-10 max-w-[300px] scale" />
       {showLinks && (
         <div className="z-10 ml-12 hidden items-center gap-11 lg:flex">
